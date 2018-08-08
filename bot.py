0d1b71942df05ffa060f5e4fb6e06b9fc7305560
ousaka99 <ousaka99@gmail.com>
2018-08-08 21:58:12 +0900

    v0.9.7

--- bot.py
+++ bot.py
@@ -28,24 +28,27 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if (message.author == client.user) or (message.channel.id not in config_channel_ids):
+        # 自分自身の発言や、登録されていないチャンネルの発言は無視する。
+        return
+
+    words = message.content.split()
+    if words[0] not in commands:
+        # 登録されているコマンド以外は無視する。
         return
 
     global commandUsers
-    if len(commandUsers) > 1:
-        commandUsers.pop(0)
-
-    for command in commands:
-        if message.content.startswith(command):
-            if message.author.voice_channel is None:
-                if message.author not in commandUsers:
-                    msg = f'すみません。よく聞き取れませんでした。ボイスチャンネルに入っていただけるとよく聞こえるのですが、、、'
-                    await client.send_message(message.channel, msg)
-                    commandUsers.append(message.author)
-                    return
-                elif 0 == random.choice(range(2)):
-                    msg = f'すみません。よく聞き取れませんでした。もう一度お願いします。'
-                    await client.send_message(message.channel, msg)
-                    return
+    if message.author.voice_channel is None:
+        if message.author not in commandUsers:
+            if 0 == random.choice(range(2)):
+                msg = f'すみません。今気づきました。ボイスチャンネルに入っていただけるとすぐ気づくのですが、、、'
+                commandUsers.append(message.author)
+                await client.send_message(message.channel, msg)
+                return
+        elif 0 == random.choice(range(4)):
+            msg = f'すみません。よく聞き取れませんでした。続けてもう一度お願いします。'
+            commandUsers.append(message.author)
+            await client.send_message(message.channel, msg)
+            return
 
     if message.author.voice_channel is None:
         voice_channel_name = 'None'
@@ -53,21 +56,29 @@ async def on_message(message):
         voice_channel_name = message.author.voice_channel.name
 
     if message.content.startswith(config_command_tier):
-        params = message.content.split()
+        params = words[1:]
         min_tier = -1
         max_tier = -1
-        if len(params) >= 3:
+        options = params[2:]
+        comment = ""
+        if len(params) >= 2:
             try:
-                min_tier = int(params[1])
-                max_tier = int(params[2])
+                min_tier = int(params[0])
+                max_tier = int(params[1])
             except ValueError:
                 # PythonにはTryParseが無いため、実際にキャストしてみる(エラーは握り潰し)
                 pass
+        for option in options:
+            if option.startswith("-c"):
+                comment = option[2:]
 
         if (1 <= min_tier <= 10) and (1 <= max_tier <= 10) and min_tier <= max_tier:
             tiers = list(range(min_tier, max_tier + 1))
             tier = random.choice(tiers)
-            msg = f'Tier{tier} がいいと思います。\n' + \
+            msg = ''
+            if len(comment) > 0:
+                msg = f"{comment}\n"
+            msg += f'Tier{tier} がいいと思います。' + \
                 f'from {voice_channel_name}'
             await client.send_message(message.channel, msg)
         else:
@@ -79,38 +90,43 @@ async def on_message(message):
             return
 
     elif message.content.startswith(config_command_ship):
-        params = message.content.split()
+        params = words[1:]
         min_tier = -1
         max_tier = -1
+        options = []    # 変動
         request_count = 1
         kinds = []
-        if len(params) >= 3:
+        comment = ""
+        if len(params) >= 2:
             try:
-                min_tier = int(params[1])
-                max_tier = int(params[2])
+                min_tier = int(params[0])
+                max_tier = int(params[1])
             except ValueError:
                 # PythonにはTryParseが無いため、実際にキャストしてみる(エラーは握り潰し)
                 pass
+        if len(params) >= 3:
+            try:
+                request_count = int(params[2])
+            except ValueError:
+                # PythonにはTryParseが無いため、実際にキャストしてみる(エラーは握り潰し)
+                request_count = -1
+            if request_count > 0:
+                options = params[3:]
+            else:
+                request_count = 1
+                options = params[2:]
 
-        if len(params) >= 4:
-            options = params[3:]
-
-            for option in options:
-                try:
-                    request_count = int(option)
-                except ValueError:
-                    # PythonにはTryParseが無いため、実際にキャストしてみる(エラーは握り潰し)
-                    pass
-
-            for option in options:
-                if 'CV' in option:
-                    kinds.append('空母')
-                if 'BB' in option:
-                    kinds.append('戦艦')
-                if 'CA' in option:
-                    kinds.append('巡洋')
-                if 'DD' in option:
-                    kinds.append('駆逐')
+        for option in options:
+            if option.startswith("-c"):
+                comment = option[2:]
+            if 'CV' in option:
+                kinds.append('空母')
+            if 'BB' in option:
+                kinds.append('戦艦')
+            if 'CA' in option:
+                kinds.append('巡洋')
+            if 'DD' in option:
+                kinds.append('駆逐')
 
         if request_count > 20:
             msg = f'すみません。欲張りすぎです。もうちょっと少なくしてください。'
@@ -148,8 +164,10 @@ async def on_message(message):
                 name = x['name']
                 tier = x['tier']
                 ships.append(f'{name}(Tier{tier})')
-
-            msg = '\n'.join(ships) + '\nがいいと思います。' + \
+            msg = ''
+            if len(comment) > 0:
+                msg = f"{comment}\n"
+            msg += '\n'.join(ships) + '\nがいいと思います。' + \
                 f'from {voice_channel_name}'
             await client.send_message(message.channel, msg)
         else:
@@ -161,13 +179,24 @@ async def on_message(message):
             return
 
     elif message.content.startswith(config_command_choice):
-        params = message.content.split()
-        if len(params) >= 2:
-            choices = params[1:]
+        params = words[1:]
+        options = params[0:]
+        choices = []
+        comment = ""
 
+        for option in options:
+            if option.startswith("-c"):
+                comment = option[2:]
+            else:
+                choices.append(option)
+
+        if len(choices) >= 1:
             choice = random.choice(choices)
-            msg = f'{choice}がいいと思います。\n' + \
-                  f'from {voice_channel_name}'
+            msg = ''
+            if len(comment) > 0:
+                msg = f"{comment}\n"
+            msg += f'{choice} がいいと思います。' + \
+                f'from {voice_channel_name}'
             await client.send_message(message.channel, msg)
         else:
             msg = f'すみません。よく分かりませんでした。' + \
@@ -178,21 +207,30 @@ async def on_message(message):
             return
 
     elif message.content.startswith(config_command_pickup):
-        params = message.content.split()
+        params = words[1:]
+        options = params[1:]
+        choices = []
         pickup_count = -1
-        if len(params) >= 2:
+        comment = ""
+        if len(params) >= 1:
             try:
-                pickup_count = int(params[1])
+                pickup_count = int(params[0])
             except ValueError:
                 # PythonにはTryParseが無いため、実際にキャストしてみる(エラーは握り潰し)
                 pass
 
-        if len(params) >= 3:
-            choices = params[2:]
+        for option in options:
+            if option.startswith("-c"):
+                comment = option[2:]
+            else:
+                choices.append(option)
 
         if 0 < pickup_count <= len(choices):
             pickups = random.sample(choices, pickup_count)
-            msg = '\n'.join(pickups) + '\nがいいと思います。' + \
+            msg = ''
+            if len(comment) > 0:
+                msg = f"{comment}\n"
+            msg += '\n'.join(pickups) + '\nがいいと思います。' + \
                 f'from {voice_channel_name}'
             await client.send_message(message.channel, msg)
         else:
